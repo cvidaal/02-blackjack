bbcda8a8fdba7b3a1b661e0424ff5c6e61bebc77
Carlos Vidal <cvidals.dev@gmail.com>
2025-10-30 16:59:03 +0100

    Usar módulos y optimizaciones del código (juego.js) y archivo -min.js

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,140 +1,155 @@
-/**
-* 2C =.. Two of Clubs (Tréboles)
-* 2D =.. Two of Diamonds (Diamantes)
-* 2H =.. Two of Hearts (Corazones)
-* 2S =.. Two of Spades (Espadas)
-*/
+// Patrón Módulo
+// funciones anónimas autoejecutables
+const miModulo = (() => {
+  "use-strict"; // Modo estricto
 
-// Variables
-let deck = [];
-const tipos = ['C', 'D', 'H', 'S'];
-const especiales = ['A', 'J', 'Q', 'K']
+  // Variables
+  let deck = [];
+  const tipos = ["C", "D", "H", "S"],
+    especiales = ["A", "J", "Q", "K"];
 
-let puntosJugador = 0;
-let puntosIA = 0;
+  const puntosJugadores = []; // Arreglo de puntos
 
-// Referencias de HTML
-const btnPedir = document.querySelector('#btnPedirCarta');
-const btnDetener = document.querySelector('#btnDetener');
-const btnNuevo = document.querySelector('#btnNuevo');
+  // Referencias de HTML
+  const btnPedir = document.querySelector("#btnPedirCarta"),
+    btnDetener = document.querySelector("#btnDetener"),
+    btnNuevo = document.querySelector("#btnNuevo");
 
-const divCartasJugador = document.querySelector('#jugador-cartas');
-const divCartasIA = document.querySelector('#ia-cartas');
-const puntosHTML = document.querySelectorAll('small');
+  const divCartasJugadores = document.querySelectorAll(".divCartas"),
+    puntosHTML = document.querySelectorAll("small");
 
-// Crear baraja
-const crearDeck = () => {
-    for( let i = 2; i<=10; i++ ) {
-        for(let tipo of tipos){ // Recorrer arreglo
-            deck.push( i + tipo ); // Agregar elemento a un arreglo
-        }
-    }
-
-    for (let tipo of tipos){
-        for(let esp of especiales){
-            deck.push(esp + tipo);
-        }
-    }
-    // console.log(deck);
-    deck = _.shuffle( deck ); // Uso de la libreria underscore para mezclar el arreglo
-    return deck;
-}
-crearDeck();
-
-// Función para tomar una carta
-const pedirCarta = () => {
-    if (deck.length === 0) {
-        throw 'No hay cartas en el deck';
-    }; // Validar que el deck no esté vacío
-
-    const carta = deck.pop();
-    return carta;
-}
-//pedirCarta();
-const valorCarta = (carta) => {
-    const valor = carta.substring(0, carta.length - 1); // Primera posición del string
-    return isNaN(valor) ?
-        (valor === 'A') ? 11 : 10
-        : valor * 1; // Si no es un número, es una letra (A,J,Q,K)
-}
-
-// Turno de la IA
-const turnoIA = (puntosMinimos) => {
-    do {
-        const carta = pedirCarta();
-        puntosIA += valorCarta(carta);
-        puntosHTML[1].innerText = puntosIA;
-
-        // <img class="carta" src="assets/cartas/10C.png" alt="cartas">
-        const imgCarta = document.createElement("img");
-        imgCarta.src = `assets/cartas/${carta}.png`;
-        imgCarta.classList.add("carta");
-        divCartasIA.append(imgCarta);
-
-        if (puntosMinimos > 21){
-            break;
-        }
-
-    } while((puntosIA < puntosMinimos) && (puntosMinimos <= 21));
-
-    // Determinar ganador
-    setTimeout(() => {
-        if (puntosIA === puntosMinimos) {
-        alert("Nadie gana (EMPATE)");
-        } else if (puntosMinimos <= 21 && puntosIA > 21) {
-        alert("Jugador GANA");
-        } else {
-        alert("IA GANA");
-        }
-    }, 10);
-
-}
-
-// Eventos
-btnPedir.addEventListener('click', () => {
-    const carta = pedirCarta();
-    puntosJugador += valorCarta( carta );
-    puntosHTML[0].innerText = puntosJugador;
-
-    // <img class="carta" src="assets/cartas/10C.png" alt="cartas">
-    const imgCarta = document.createElement('img');
-    imgCarta.src = `assets/cartas/${carta}.png`;
-    imgCarta.classList.add('carta');
-
-    divCartasJugador.append(imgCarta);
-
-    if (puntosJugador > 21) {
-        console.warn('Lo siento mucho, perdiste');
-        btnPedir.disabled = true; // Deshabilitar botón
-        turnoIA(puntosJugador); // Llamar a la función de la IA si el jugador se pasa de 21
-    } else if (puntosJugador === 21) {
-        console.warn('21, genial!');
-        btnPedir.disabled = true; // Deshabilitar botón
-    }
-});
-
-btnDetener.addEventListener('click', () => {
-    btnPedir.disabled = true;
-    btnDetener.disabled = true;
-    turnoIA( puntosJugador );
-})
-
-btnNuevo.addEventListener('click', () => {
-    deck = [];
+  // Inicializar juego
+  const inicializarJuego = (numJugadores = 2) => {
     deck = crearDeck();
-    puntosJugador = 0;
-    puntosIA = 0;
+    puntosJugadores.length = 0; // Reiniciar arreglo de puntos
 
+    for (let i = 0; i < numJugadores; i++) {
+      puntosJugadores.push(0);
+    }
     // Limpiar puntos de jugador y de la IA
-    puntosHTML[0].innerText = 0;
-    puntosHTML[1].innerText = 0;
+    puntosHTML.forEach((elem) => (elem.innerText = 0));
 
     // Limpiar las cartas del jugador y de la IA
-    divCartasJugador.innerHTML = '';
-    divCartasIA.innerHTML = '';
+    divCartasJugadores.forEach((elem) => (elem.innerHTML = ''));
 
     // Habilitar botones
     btnPedir.disabled = false;
     btnDetener.disabled = false;
+  };
 
-});
+  // Crear baraja
+  const crearDeck = () => {
+    deck = []; // Reiniciar deck
+
+    for (let i = 2; i <= 10; i++) {
+      for (let tipo of tipos) {
+        // Recorrer arreglo
+        deck.push(i + tipo); // Agregar elemento a un arreglo
+      }
+    }
+
+    for (let tipo of tipos) {
+      for (let esp of especiales) {
+        deck.push(esp + tipo);
+      }
+    }
+
+    return _.shuffle(deck); // Mezclar deck usando Lodash
+  };
+
+  // Función para tomar una carta
+  const pedirCarta = () => {
+    if (deck.length === 0) {
+      throw "No hay cartas en el deck";
+    } // Validar que el deck no esté vacío
+
+    return deck.pop();
+  };
+
+  //pedirCarta();
+  const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1); // Primera posición del string
+    return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1; // Si no es un número, es una letra (A,J,Q,K)
+  };
+
+  // Turno: 0 = primer jugador y el último será la IA
+  const acumularPuntos = (carta, turno) => {
+    puntosJugadores[turno] += valorCarta(carta);
+    puntosHTML[turno].innerText = puntosJugadores[turno];
+    return puntosJugadores[turno];
+  };
+
+  // Crear carta
+  const crearCarta = (carta, turno) => {
+    const imgCarta = document.createElement("img");
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add("carta");
+    divCartasJugadores[turno].append(imgCarta);
+  };
+
+  // Determinar ganador
+  const determinarGanador = () => {
+    const [puntosMinimos, puntosIA] = puntosJugadores; // Desestructuración de arreglos
+
+        setTimeout(() => {
+          if (puntosIA === puntosMinimos) {
+            alert("Nadie gana (EMPATE)");
+          } else if (puntosMinimos <= 21 && puntosIA > 21) {
+            alert("Jugador GANA");
+          } else {
+            alert("IA GANA");
+          }
+        }, 100);
+  }
+
+  // Turno de la IA
+  const turnoIA = (puntosMinimos) => {
+    let puntosIA = 0;
+    do {
+      const carta = pedirCarta();
+      puntosIA = acumularPuntos(carta, puntosJugadores.length - 1);
+      crearCarta(carta, puntosJugadores.length - 1);
+
+      if (puntosMinimos > 21) {
+        break;
+      }
+    } while (puntosIA < puntosMinimos && puntosMinimos <= 21);
+
+    // Determinar ganador
+    determinarGanador();
+  };
+
+  // Eventos
+  btnPedir.addEventListener("click", () => {
+    const carta = pedirCarta();
+    const puntosJugador = acumularPuntos(carta, 0);
+
+    crearCarta(carta, 0);
+
+    if (puntosJugador > 21) {
+      console.warn("Lo siento mucho, perdiste");
+      btnPedir.disabled = true; // Deshabilitar botón
+      turnoIA(puntosJugador); // Llamar a la función de la IA si el jugador se pasa de 21
+    } else if (puntosJugador === 21) {
+      console.warn("21, genial!");
+      btnPedir.disabled = true; // Deshabilitar botón
+    }
+  });
+
+  btnDetener.addEventListener("click", () => {
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoIA(puntosJugadores[0]);
+  });
+
+//   btnNuevo.addEventListener("click", () => {
+//     inicializarJuego(2);
+
+//   });
+
+    // Esta es la última parte del módulo que debo ejecutarlo para tener acceso en el index.html
+    // Todo lo que ponga aquí sera público
+    return {
+        nuevoJuego : inicializarJuego, // Exponer la función para que sea público.
+    };
+})();
